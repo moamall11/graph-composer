abcc2054b364a9cb4ff97e9b700a40fa7c8245e7
moamel <moamall11@mail.ru>
2022-06-30 20:51:50 +0400

    first commit

--- compose_template.py
+++ compose_template.py
@@ -18,16 +18,64 @@ import random
 from graph import Graph, Vertex
 
 def get_words_from_text(text_path):
-    pass
+    #read the text file.
+    with open(text_path,'r') as f:
+        #get the text from the file.
+        text = f.read()
+        #make it lower case to avoid confusion.
+        text = text.lower()
+        #remove the punctuation (we want only the words to avoid confusion).
+        text = text.translate(str.maketrans('','',string.punctuation))
+        # to remove [verse 1: artist] when doing lyrics.
+        #text = re.sub(r'\[(.+)\]', ' ', text)
+        #make a list of the words.
+        #remove the white spaces.
+        words = text.split()
+
+    return words
+
 
 def make_graph(words):
-    pass
+    #make an instance of the graph.
+    g = Graph()
+    #initialize a value (in the beginning there is no previous word).
+    previous_word = None
+    #go through the words.
+    for word in words:
+        #get the vertex of the word.
+        word_vertex = g.get_vertex(word)
+        #increase the edge of this word in the previous word.
+        if previous_word:
+            previous_word.increment_edge(word_vertex)
+        #set this word as the previous word.
+        previous_word = word_vertex
+    #generate the two lists(neighboring verticies,weights)for each vertex.
+    g.generate_probability_mappings()
+
+    return g
+
 
 def compose(g, words, length=50):
-    pass
+    # pick the first word randomly and get it's vertex..val
+    word_vertex = g.get_vertex(random.choice(words))
+    #print(word_vertex.neighboring_verticies)
+    #initialize a list.
+    composition = []
+    #get the next word and add it to the composition 'length' times.
+    for _ in range(length):
+        composition.append(word_vertex.value)
+        word_vertex = g.get_next_word(word_vertex)
+    return composition
 
 def main():
-    pass
+    #get the list of the words all in lowercase 
+    #without whitespaces and punctuation.
+    words = get_words_from_text("texts/hp_sorcerer_stone.txt")
+    #get the graph.
+    g = make_graph(words)
+    #get the list of words and print them.
+    composition = compose(g,words,100)
+    print(' '.join(composition))
 
 
 if __name__ == '__main__':
--- graph_template.py
+++ graph_template.py
@@ -15,41 +15,72 @@ import random
 
 class Vertex(object):
     def __init__(self, value):
-        pass
+        #the value is the word.
+        self.value = value
+        #make a dictionary that includes all the words 
+        #that have an edge from this vertex. 
+        self.adjacent = {}
+        self.neighboring_verticies = []
+        self.neighboring_weights = []
 
     def add_edge_to(self, vertex, weight=0):
-        pass
-
+        #assign an edge to a vertex.
+        self.adjacent[vertex] = weight
+ 
     def increment_edge(self, vertex):
-        pass
+        #increase the edge of a vertex.
+        self.adjacent[vertex] = self.adjacent.get(vertex,0) + 1
 
     def get_adjacent_nodes(self):
-        pass
+        return self.adjacent.keys()
 
     # initializes probability map
     def get_probability_map(self):
-        pass
+        #make two lists from the dictionary.
+        for vertex,weight in self.adjacent.items():
+            self.neighboring_verticies.append(vertex)
+            self.neighboring_weights.append(weight)
+
 
     def next_word(self):
-        pass
-
-
+        #returns the most probable vertex depending on the weights.
+        # return random.choices(self.neighboring_verticies,
+            # weights = self.neighboring_weights)[0]
+        #go through all the neighboring verticies 
+        #until u find the one with the highest weight and return it.
+        score=0
+        for i,vertex in enumerate(self.neighboring_verticies):
+            if self.neighboring_weights[i] >= score:
+                next_word_vertex = vertex
+                score = self.neighboring_weights[i]
+        return next_word_vertex
 
 class Graph(object):
     def __init__(self):
-        pass
+        #make a dictionary of all the words and their verticies objects.
+        self.verticies = {}
 
     def get_vertex_values(self):
-        pass
+        #return all the possible words to be used.
+        return self.verticies.keys()
 
     def add_vertex(self, value):
-        pass
+        #take a value(word) and convert it into a vertex object 
+        #and add them to the dictionary.
+        self.verticies[value]=Vertex(value)
+
 
     def get_vertex(self, value):
-        pass
+        #return the vertex object of a value(word) 
+        #and if it doesn't exist add one.
+        if value not in self.verticies:
+            self.add_vertex(value)
+        return self.verticies[value]
 
     def get_next_word(self, current_vertex):
-        pass
+        #returns the next vertex.
+        return current_vertex.next_word()
 
     def generate_probability_mappings(self):
-        pass
+        for vertex in self.verticies.values():
+            vertex.get_probability_map()
